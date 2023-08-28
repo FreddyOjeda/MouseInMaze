9bd02e1205f0d680a588a9d997f3c4550db878e0
FreddyOjeda <freddy.ojeda@uptc.edu.co>
2023-08-27 22:45:39 -0500

    Recorridos de matriz completados

---
+++ ArrayMaker.py
@@ -0,0 +1,79 @@
+from PIL import Image
+
+def load_image_and_convert_to_matrix(image_path):
+    img = Image.open(image_path)
+    img = img.convert("L")  # Convertir a escala de grises
+    img = img.point(lambda p: p < 128 and 1)  # Convertir a 1s (paredes) y 0s (espacios libres)
+
+    width, height = img.size
+    matrix = []
+    for y in range(height):
+        row = []
+        for x in range(width):
+            pixel = img.getpixel((x, y))
+            row.append(pixel)
+        matrix.append(row)
+
+    return matrix
+
+def is_redish(pixel):
+    # Verifica si el pixel tiene tonalidades de rojo
+    r, g, b = pixel
+    return r > 150 and g < 100 and b < 100
+
+
+def is_yellowish(pixel):
+    # Verifica si el pixel tiene tonalidades de amarillo
+    r, g, b = pixel
+    return r > 150 and g > 150 and b < 100
+
+def is_light_gray(pixel):
+    # Verifica si el pixel tiene tonalidades de blanco (gris claro)
+    r, g, b = pixel
+    return r > 200 and g > 200 and b > 200
+
+def is_dark_gray(pixel):
+    # Verifica si el pixel tiene tonalidades de negro (gris oscuro)
+    r, g, b = pixel
+    return r < 50 and g < 50 and b < 50
+
+def load_image_and_convert_to_matrix2(image_path):
+    img = Image.open(image_path)
+    img = img.convert("RGB")  # Convertir a formato RGB
+
+    width, height = img.size
+    matrix = []
+    for y in range(height):
+        row = []
+        for x in range(width):
+            pixel = img.getpixel((x, y))
+            if is_light_gray(pixel):  # Tonos de blanco (gris claro)
+                row.append(0)
+            elif is_dark_gray(pixel):  # Tonos de negro (gris oscuro)
+                row.append(1)
+            elif is_redish(pixel):  # Tonos de rojo
+                row.append(2)
+            elif is_yellowish(pixel):  # Tonos de amarillo
+                row.append(3)
+            else:
+                row.append(-1)  # Valor negativo para otros colores no especificados
+        matrix.append(row)
+
+    return matrix
+
+def print_matrix(matrix):
+    for row in matrix:
+        print(" ".join(map(str, row)))
+
+def export_matrix_to_txt(matrix, output_file):
+    with open(output_file, "w") as f:
+        for row in matrix:
+            f.write(" ".join(map(str, row)) + "\n")
+
+if __name__ == "__main__":
+    image_path = "./images/pixil.png"
+    matrix = load_image_and_convert_to_matrix2(image_path)
+    output_file = "matrix_output2.txt"  # Cambia el nombre del archivo si lo deseas
+    export_matrix_to_txt(matrix, output_file)
+
+
---
+++ way.py
@@ -0,0 +1,61 @@
+
+def find_path(matrix, start, end):
+    def dfs(x, y):
+        if x < 0 or y < 0 or x >= len(matrix[0]) or y >= len(matrix) or visited[y][x] or matrix[y][x] == 1:
+            return False
+        
+        path.append((x, y))
+        print(path)
+        visited[y][x] = True
+
+        if (x, y) == end:
+            return True
+        
+        if dfs(x + 1, y) or dfs(x - 1, y) or dfs(x, y + 1) or dfs(x, y - 1):
+            return True
+        
+        path.pop()
+        return False
+
+    path = []
+    visited = [[False] * len(matrix[0]) for _ in range(len(matrix))]
+    dfs(start[0], start[1])
+    return path
+
+def load_matrix_from_file(file_path):
+    matrix = []
+    with open(file_path, "r") as f:
+        for line in f:
+            row = [int(cell) for cell in line.strip().split()]
+            matrix.append(row)
+    return matrix
+
+def find_coordinates(matrix, value):
+    for y, row in enumerate(matrix):
+        for x, cell in enumerate(row):
+            if cell == value:
+                return x, y
+    return None, None
+
+
+# Define la matriz y los puntos de inicio y final
+matrix = load_matrix_from_file("./matrix_output2.txt")
+
+x_start, y_start = find_coordinates(matrix, 2)  # Buscar el punto de inicio (2)
+x_end, y_end = find_coordinates(matrix, 3) 
+start_point = (x_start, y_start)
+end_point = (x_end, y_end)
+#start_point=(56, 26)
+#end_point=(6,61)
+print("Punto de inicio" , start_point)
+print("Punto de llegada: ",end_point)
+path = find_path(matrix, start_point, end_point)
+
+if path:
+    print("Camino encontrado:")
+    for x, y in path:
+        matrix[y][x] = "X"  # Marcar el camino en la matriz
+    for row in matrix:
+        print(" ".join(map(str, row)))
+else:
+    print("No se encontró un camino válido.")
