fa6ec636518bb74313bb163ca5255f7ed4bef73f
Diego Corredor Dev <diego.corredor.98@gmail.com>
2023-09-25 20:34:09 -0500

    last commit

--- test.py
+++ test.py
@@ -8,7 +8,6 @@ import time
 window = tk.Tk()
 window.title("Proyecto de inteligencia artificial generativa - IAG")
 
-# Crear los frames para las columnas
 frame_col1 = tk.Frame(window)
 frame_col2 = tk.Frame(window)
 frame_col3 = tk.Frame(window)
@@ -34,21 +33,19 @@ def solve_mazes():
     solve_bfs()
     solve_a_star()
 
-# Agregar contenido a la columna 1
-label_left_top = tk.Label(frame_col1, text="Por favor seleccione el \n laberinto a solucionar:", **text_style)
-button_select_image = tk.Button(frame_col1, text="Seleccionar Imagen", command=select_image)
+label_left_top = tk.Label(frame_col1, text="Por favor seleccione el laberinto a solucionar:", **text_style)
+button_select_image = tk.Button(frame_col1, text="Seleccionar Imagen", command=select_image, **button_style)
 button_solve_mazes = tk.Button(frame_col1, text="Resolver Laberintos", command=solve_mazes, **button_style)
-label_members = tk.Label(frame_col1, text="Integrandes del grupo:\n\n- Mayerly Andrea Salcedo\n- Diego Alejandro Corredor Rojas\n- Freddy Esteban Ojeda Cogua", **text_style)
+label_members = tk.Label(frame_col1, text="Integrantes del grupo:\n\n- Mayerly Andrea Salcedo\n- Diego Alejandro Corredor Rojas\n- Freddy Esteban Ojeda Cogua", **text_style)
 
-label_left_top.pack()
-button_select_image.pack()
-button_solve_mazes.pack()
-label_members.pack()
+label_left_top.pack(pady=10)
+button_select_image.pack(pady=10)
+button_solve_mazes.pack(pady=10)
+label_members.pack(pady=10)
 
-# Crear los canvas para mostrar las imágenes en las columnas 2, 3 y 4
-canvas_col2 = tk.Canvas(frame_col2, width=500, height=800)  # Ajusta el ancho y alto según tus necesidades
-canvas_col3 = tk.Canvas(frame_col3, width=500, height=800)  # Ajusta el ancho y alto según tus necesidades
-canvas_col4 = tk.Canvas(frame_col4, width=500, height=800)  # Ajusta el ancho y alto según tus necesidades
+canvas_col2 = tk.Canvas(frame_col2, width=500, height=560)
+canvas_col3 = tk.Canvas(frame_col3, width=500, height=560)
+canvas_col4 = tk.Canvas(frame_col4, width=500, height=560)
 
 canvas_col2.pack()
 canvas_col3.pack()
@@ -95,16 +92,13 @@ cam1 = read_coordinates_from_file("path_dfs.txt")
 cam2 = read_coordinates_from_file("path_bfs.txt")
 cam3 = read_coordinates_from_file("path_a_star.txt")
 
-maze_width = len(laberinto[0])  # Ancho del laberinto
-maze_height = len(laberinto)     # Alto del laberinto
-
 camino1 = invert_coordinates(cam1)
 camino2 = invert_coordinates(cam2)
 camino3 = invert_coordinates(cam3)
 cell_size = 15
 
 def get_simulation_images(laberinto, caminito, canvas):
-    canvas.delete("all")  # Limpiar el canvas antes de dibujar
+    canvas.delete("all")
     for i in range(len(laberinto)):
         for j in range(len(laberinto[i])):
             x1 = j * cell_size
@@ -118,32 +112,22 @@ def get_simulation_images(laberinto, caminito, canvas):
                 canvas.create_rectangle(x1, y1, x2, y2, fill="white")
     animate_path(caminito, canvas)
 
-# Dentro de la función show_simulation_images
 def show_simulation_images(path_image):
     laberinto_image32 = arrayMaker(path_image)
+    get_simulation_images(laberinto, camino1, canvas_col2)
+    get_simulation_images(laberinto, camino2, canvas_col3)
+    get_simulation_images(laberinto, camino3, canvas_col4)
 
-    # Llamar a get_simulation_images con los caminos y canvas
-    cam1 = read_coordinates_from_file("path_dfs.txt")
-    get_simulation_images(laberinto, cam1, canvas_col2)
-
-    cam2 = read_coordinates_from_file("path_bfs.txt")
-    get_simulation_images(laberinto, cam2, canvas_col3)
-
-    cam3 = read_coordinates_from_file("path_a_star.txt")
-    get_simulation_images(laberinto, cam3, canvas_col4)
-
-    # Configurar las imágenes en los cuadros correspondientes
-    label_col2 = tk.Label(frame_col2, image=laberinto_image32, text="Algortimo a")
-    label_col3 = tk.Label(frame_col3, image=laberinto_image32, text="Algortimo b")
-    label_col4 = tk.Label(frame_col4, image=laberinto_image32, text="Algortimo c")
+    label_col2 = tk.Label(frame_col2, image=laberinto_image32, text="Algoritmo DFS")
+    label_col3 = tk.Label(frame_col3, image=laberinto_image32, text="Algoritmo BFS")
+    label_col4 = tk.Label(frame_col4, image=laberinto_image32, text="Algoritmo A*")
 
     label_col2.image = laberinto_image32
     label_col3.image = laberinto_image32
     label_col4.image = laberinto_image32
 
-    label_col2.pack()
-    label_col3.pack()
-    label_col4.pack()
+    label_col2.pack(pady=10, anchor="center")
+    label_col3.pack(pady=10, anchor="center")
+    label_col4.pack(pady=10, anchor="center")
 
-# Iniciar el bucle principal de la interfaz gráfica
 window.mainloop()
